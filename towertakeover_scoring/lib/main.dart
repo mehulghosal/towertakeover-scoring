cb939ad9a0d250544b36132f30cb4206d97c0fc5
mehulghosal <mehul.ghosal@gmail.com>
2019-05-04 21:29:03 -0400

    woah its like idk what im doing

--- towertakeover_scoring/lib/main.dart
+++ towertakeover_scoring/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(new MyApp());
+  });
+}
+//void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -25,49 +32,242 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  double b = -.53;
+
   int _counter = 0;
+//  by default, false is blue
+  bool alliance = false;
+
+  int redOrange = 0;
+  int redGreen  = 0;
+  int redPurple = 0;
+
+  int blueOrange = 0;
+  int blueGreen  = 0;
+  int bluePurple = 0;
+
+//  high scored cubes
+  int highOrange = 0;
+  int highGreen  = 0;
+  int highPurple = 0;
 
   void hold() {
     print("press");
   }
 
+//  CALCULATE BLUE ALLIANCE SCORE
+  int blueScore(){
+    return highOrange*blueOrange + highGreen*blueGreen + highPurple+bluePurple;
+  }
+//  CALCULATE RED ALLIANCE SCORE
+  int redScore(){
+    return highOrange*redOrange + highGreen*redGreen + highPurple+redPurple;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      
       body: new Stack(
         children: <Widget>[
-//          red
+
+//         ALLIANCE COLOR SELECTION
           Container(
-            alignment: Alignment(-2, -1),
-            child: Center(
-              child: ButtonBar(
-                children: <Widget>[
-                  Text("Blue Cubes"),
-                  MaterialButton(
-                    color: Colors.orange,
-                    child: Text("+",
-                        style: new TextStyle(fontSize: 20)),
-                    onPressed: hold,
-                  ),
-                  MaterialButton(
-                    color: Colors.green,
-                    child: Text("+",
-                        style: new TextStyle(fontSize: 20)),
-                    onPressed: hold,
-                  ),
-                  MaterialButton(
-                    color: Colors.purple,
-                    child: Text("+",
-                        style: new TextStyle(fontSize: 20)),
-                    onPressed: hold,
-                  ),
-                ],
-              ),
-            )
+            alignment: Alignment(0.1, -0.95),
+            child: Row(
+//              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Alliance Color Selector (blue is default)", style: new TextStyle(fontSize: 21),),
+                Checkbox(
+                  value: alliance,
+                  onChanged: (bool value) {
+                    setState(() {
+                      alliance = value;
+                      print(alliance);
+                    });
+                  },
+                ),
+              ],
+            ),
           ),
+//          RED CUBES
+          Container(
+            alignment: Alignment(0, -0.82),
+            child: Text("Red Alliance Cubes", style: new TextStyle(fontSize: 19),)
+          ),
+//          ADDING
+          Container(
+            alignment: Alignment(0, -0.75),
+            child: Row(
+              children: <Widget>[
+               Expanded(
+                  child:MaterialButton(
+                     color: Colors.orange,
+                     child: Text("+", style: new TextStyle(fontSize: 20)),
+                     onPressed: (){
+                       redOrange+=1;
+                     },
+                  ),
+               ),
+               Expanded(
+                 child:MaterialButton(
+                   color: Colors.green,
+                   child: Text("+", style: new TextStyle(fontSize: 20)),
+                   onPressed: (){
+                     redGreen+=1;
+                   },
+                 ),
+               ),
+               Expanded(
+                 child:MaterialButton(
+                   color: Colors.purple,
+                   child: Text("+", style: new TextStyle(fontSize: 20)),
+                   onPressed: (){
+                     redPurple+=1;
+                   },
+                 ),
+               ),
+              ],
+            ),
+          ),
+//          SUBTRACTING
+          Container(
+            alignment: Alignment(0, -0.68),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child:MaterialButton(
+                    color: Colors.orange,
+                    child: Text("-", style: new TextStyle(fontSize: 20)),
+                    onPressed: (){
+                      redOrange-=1;
+                    },
+                  ),
+                ),
+                Expanded(
+                  child:MaterialButton(
+                    color: Colors.green,
+                    child: Text("-", style: new TextStyle(fontSize: 20)),
+                    onPressed: (){
+                      redGreen-=1;
+                    },
+                  ),
+                ),
+                Expanded(
+                  child:MaterialButton(
+                    color: Colors.purple,
+                    child: Text("-", style: new TextStyle(fontSize: 20)),
+                    onPressed: (){
+                      redPurple-=1;
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+//          RED NUMBER OF CUBES
+          Container(
+            alignment: Alignment(0, -.575),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text("Orange Cubes: " + redOrange.toString())
+                ),
+                Expanded(
+                    child: Text("Green Cubes: " + redGreen.toString())
+                ),
+                Expanded(
+                    child: Text("Purple Cubes: " + redPurple.toString())
+                ),
+              ],
+            ),
+          ),
+
+//          BLUE CUBES
+          Container(
+              alignment: Alignment(0, b),
+              child: Text("Blue Alliance Cubes", style: new TextStyle(fontSize: 19),)
+          ),
+//          ADDING
+          Container(
+            alignment: Alignment(0, b + 0.07),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child:MaterialButton(
+                    color: Colors.orange,
+                    child: Text("+", style: new TextStyle(fontSize: 20)),
+                    onPressed: hold,
+                  ),
+                ),
+                Expanded(
+                  child:MaterialButton(
+                    color: Colors.green,
+                    child: Text("+", style: new TextStyle(fontSize: 20)),
+                    onPressed: hold,
+                  ),
+                ),
+                Expanded(
+                  child:MaterialButton(
+                    color: Colors.purple,
+                    child: Text("+", style: new TextStyle(fontSize: 20)),
+                    onPressed: hold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+//          SUBTRACTING
+          Container(
+            alignment: Alignment(0, b + .14),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child:MaterialButton(
+                    color: Colors.orange,
+                    child: Text("-", style: new TextStyle(fontSize: 20)),
+                    onPressed: hold,
+                  ),
+                ),
+                Expanded(
+                  child:MaterialButton(
+                    color: Colors.green,
+                    child: Text("-", style: new TextStyle(fontSize: 20)),
+                    onPressed: hold,
+                  ),
+                ),
+                Expanded(
+                  child:MaterialButton(
+                    color: Colors.purple,
+                    child: Text("-", style: new TextStyle(fontSize: 20)),
+                    onPressed: hold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+//         BLUE NUMBER CUBES
+          Container(
+            alignment: Alignment(0, b+.245),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: Text("Orange Cubes: " + blueOrange.toString())
+                ),
+                Expanded(
+                    child: Text("Green Cubes: " + blueGreen.toString())
+                ),
+                Expanded(
+                    child: Text("Purple Cubes: " + bluePurple.toString())
+                ),
+              ],
+            ),
+          ),
+
+
         ],
       ),
     );
